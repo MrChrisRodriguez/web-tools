feac3edb9022e2b995cb8a69908c4c0c5f7a9b71
Christian Rodriguez <c@mrcr.org>
2014-11-03 14:05:07 -0500

    adding hotkeys

--- nimble/quick_actions.js
+++ nimble/quick_actions.js
@@ -1,26 +1,37 @@
-var elm = "<tr><td align=\"left\" style=\"vertical-align: top;\"><div class=\"nmbl-Nimblet\"><div class=\"nmbl-Nimblet-Header\"><span class=\"nmbl-Nimblet-Header-Title\">Quick Actions</span><span class=\"gwt-InlineLabel clear\"></span></div><div class=\"nmbl-Nimblet-Content\"><div class=\"ContactSocialNetworksView\"><a class=\"viewStreams\" href=\"#\" aria-hidden=\"true\" style=\"display: none;\">View streams</a> <a href=\"javascript:setLeadStatus('sourcing');\">Sourcing</a><br/><a href=\"javascript:setLeadStatus('not_qualified');\">Not Qualified</a><br/><hr/><a href=\"javascript:setNotQualifiedReason('tooBig');\">Too Big</a><br/><a href=\"javascript:setNotQualifiedReason('badRev');\">Bad Revenue Fit</a><br/><a href=\"javascript:setNotQualifiedReason('badCulture');\">Bad Culture Fit</a><br/><a href=\"javascript:setNotQualifiedReason('noContact');\">Insufficient Contact Information</a><br/></div></div></div></td></tr>"
+//<script src="https://rawgit.com/jeresig/jquery.hotkeys/master/jquery.hotkeys.js"></script>
 
-$('#rpanel > div > div > div.lastChild > table > tbody').prepend(elm)
+//////////////
+// Setting up the environment
+//////////////
 
-// -----
+// Important Elements
+var sidePanel = $("#rpanel > div > div > div.lastChild > table > tbody");
+var prevButton= $("#cpanel > div > div > div > div > table > tbody > tr:nth-child(1) > td > div > div.profileHeader > div > table > tbody > tr > td:nth-child(1) > div");
+var nextButton = $("#cpanel > div > div > div > div > table > tbody > tr:nth-child(1) > td > div > div.profileHeader > div > table > tbody > tr > td:nth-child(4) > div");
+var contactDefault = $("#contact_default");
+var contactDetails = $("#details");
+var companyID;
+var headers;
 
-var company_id = window.location.hash.split("id=")[1].split("&")[0];
-window.onhashchange = function(){
-    company_id = window.location.hash.split("id=")[1].split("&")[0];
-    console.log(company_id);
+// Establishing sane defaults
+function getDefaults(){
+    companyID = window.location.hash.split("id=")[1].split("&")[0];
+    headers = {
+        "Authorization": "Nimble token=\"5f733185-6a2b-4ee4-9fe8-f06279020520\"",
+        "X-Nimble-Company": "Burstworks%2C%20Inc.",
+        "X-Nimble-Company-Id": company_id,
+        "X-Nimble-User": "chris@burstworks.com",
+        "X-Nimble-User-Id": "5441854efaed2949f81d2124"
+    };
 }
-
-var headers = {
-    "Authorization": "Nimble token=\"5f733185-6a2b-4ee4-9fe8-f06279020520\"",
-    "X-Nimble-Company": "Burstworks%2C%20Inc.",
-    "X-Nimble-Company-Id": company_id,
-    "X-Nimble-User": "chris@burstworks.com",
-    "X-Nimble-User-Id": "5441854efaed2949f81d2124"
-};
 var success = function(){console.log('success!')}
 var error = function(){alert('Failure!')};
 
-    
+//////////////
+// Quick Action Methods
+//////////////
+
+// Nimble Requests
 function sendNimbleRequest(data){
     var request = $.ajax({
         url: "https://app.nimble.com/api/v1/contact/" + company_id + "?_method=put",
@@ -36,6 +47,12 @@ function setLeadStatus(status){
     switch(status){
         case "sourcing":
             value="Sourcing";
+            break;
+        case "qualified":
+            value="Qualified";
+            break;
+        case "engaged":
+            value="Engaged";
             break;
         case "not_qualified":
             value="Not Qualified";
@@ -62,4 +79,92 @@ function setNotQualifiedReason(reason){
             break;
     }
     sendNimbleRequest(data);
+}
+
+//////////////
+// Estbalish Shortcuts
+//////////////
+
+$.getScript('https://rawgit.com/jeresig/jquery.hotkeys/master/jquery.hotkeys.js', function(){
+    console.log('jquery.hotkeys loaded successfully');
+
+    // Navigation
+    $(document).bind('keydown', 'alt+right', function(){
+        nextButton[0].click();
+    });
+    $(document).bind('keydown', 'alt+left', function(){
+        prevButton[0].click();
+    });
+
+
+    // Lead status
+    $(document).bind('keydown', 'alt+1', function(){
+        setLeadStatus('sourcing');
+    });
+    $(document).bind('keydown', 'alt+2', function(){
+        setLeadStatus('qualified');
+    });
+    $(document).bind('keydown', 'alt+3', function(){
+        setLeadStatus('engaged');
+    });
+    $(document).bind('keydown', 'alt+0', function(){
+        setLeadStatus('not_qualified');
+    });
+
+
+    // Windows
+    $(document).bind('keydown', 'alt+c', function(){
+        contactDefault[0].click();
+        console.log('click');
+    });
+    $(document).bind('keydown', 'alt+d', function(){
+        contactDetails[0].click();
+    });
+});
+
+
+//////////////
+// Actions and listeners
+//////////////
+
+getDefaults();
+
+// Construct and insert the quick actions panel
+
+var quickActions="";
+quickActions += "<tr>";
+quickActions += "    <td align=\"left\" style=\"vertical-align: top;\">";
+quickActions += "        <div class=\"nmbl-Nimblet\">";
+quickActions += "            <div class=\"nmbl-Nimblet-Header\">";
+quickActions += "                <span class=\"nmbl-Nimblet-Header-Title\">Quick Actions<\/span>";
+quickActions += "                <span class=\"gwt-InlineLabel clear\"><\/span>";
+quickActions += "            <\/div>";
+quickActions += "            <div class=\"nmbl-Nimblet-Content\">";
+quickActions += "                <div class=\"ContactSocialNetworksView\">";
+quickActions += "                    <a class=\"viewStreams\" href=\"#\" aria-hidden=\"true\" style=\"display: none;\">View streams<\/a>";
+quickActions += "                    <a href=\"javascript:setLeadStatus('sourcing');\">Sourcing (alt+1)<\/a><br\/>";
+quickActions += "                    <a href=\"javascript:setLeadStatus('qualified');\">Qualified (alt+2)<\/a><br\/>";
+quickActions += "                    <a href=\"javascript:setLeadStatus('engaged');\">Engaged (alt+3)<\/a><br\/>";
+quickActions += "                    <a href=\"javascript:setLeadStatus('not_qualified');\">Not Qualified (alt+0)<\/a><br\/>";
+quickActions += "                    <hr\/>";
+quickActions += "                    <a href=\"javascript:setNotQualifiedReason('tooBig');\">Too Big<\/a><br\/>";
+quickActions += "                    <a href=\"javascript:setNotQualifiedReason('badRev');\">Bad Revenue Fit<\/a><br\/>";
+quickActions += "                    <a href=\"javascript:setNotQualifiedReason('badCulture');\">Bad Culture Fit<\/a><br\/>";
+quickActions += "                    <a href=\"javascript:setNotQualifiedReason('noContact');\">Insufficient Contact Information<\/a><br\/>";
+quickActions += "                    <hr\/>";
+quickActions += "                    Previous (alt+left) <br\/>";
+quickActions += "                    Next (alt+right) <br\/>";
+quickActions += "                <\/div>";
+quickActions += "            <\/div>";
+quickActions += "        <\/div>";
+quickActions += "    <\/td>";
+quickActions += "<\/tr>";
+quickActions += "";
+
+sidePanel.prepend(quickActions)
+
+// Reset defaults on page change
+window.onhashchange = function(){
+    getDefaults();
+    console.log(companyID);
 }
